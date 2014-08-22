21908770062dd8185f15fce002079881e7ffdb3f
must-not-code <test4food@me.com>
2014-08-22 16:24:05 +0400

    add post model validation specs

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,10 @@ class Post < ActiveRecord::Base
     length: {minimum: 140, maximum: 30000, too_short: 'Это вам не твиттер!', too_long: 'Слишком многа букаф!!!111'}
 
   validates :title,
-    length: {in: 4..200, message: 'Посту нужен адекватный заголовок!'}
+    length: {in: 4..200, message: 'Посту нужен заголовок адекватной длины!'}
 
   validates :game,
-    length: {in: 2..20, message: 'Пост должен относиться к какой-то игре!'}
+    length: {in: 2..30, message: 'Пост должен относиться к какой-то игре!'}
 
   has_many :comments
   belongs_to :user
