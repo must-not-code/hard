5607203ab5bce6be68b5b19e9b8404913d64c8c2
must-not-code <test4food@me.com>
2014-08-22 16:24:05 +0400

    add post model validation specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,4 +3,23 @@ require 'rails_helper'
 describe Post do
   it { should belong_to(:user) }
   it { should have_many(:comments) }
+
+  it do
+    should ensure_length_of(:title).
+      is_at_least(4).is_at_most(200).
+      with_message('Посту нужен заголовок адекватной длины!')
+  end
+
+  it do
+    should ensure_length_of(:game).
+      is_at_least(2).is_at_most(30).
+      with_message('Пост должен относиться к какой-то игре!')
+  end
+
+  it do
+    should ensure_length_of(:content).
+      is_at_least(140).is_at_most(30000).
+      with_short_message('Это вам не твиттер!').
+      with_long_message('Слишком многа букаф!!!111')
+  end
 end
