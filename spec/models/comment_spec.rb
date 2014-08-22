317ca81b0cd48efe76141939e1b9d845133f6d9f
must-not-code <test4food@me.com>
2014-08-22 16:31:08 +0400

    add validation spec of post and result models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,11 @@ require 'rails_helper'
 describe Comment do
   it { should belong_to(:user) }
   it { should belong_to(:post) }
+
+  it do
+    should ensure_length_of(:content).
+      is_at_least(4).is_at_most(3000).
+      with_short_message('Ваш комментарий недостаточно информативен.').
+      with_long_message('Недопустимое количество символов.')
+  end
 end
--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -3,4 +3,11 @@ require 'rails_helper'
 describe Result do
   it { should belong_to(:user) }
   it { should belong_to(:tournament) }
+
+  it do
+    should ensure_length_of(:content).
+      is_at_least(4).is_at_most(3000).
+      with_short_message('Ваш комментарий недостаточно информативен.').
+      with_long_message('Недопустимое количество символов.')
+  end
 end
