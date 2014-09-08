ec4ab4d7bb10eabf440d39af56c8542c95a3f00d
must-not-code <test4food@me.com>
2014-09-08 11:54:00 +0400

    add passing score to tournaments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140906165420) do
+ActiveRecord::Schema.define(version: 20140908073225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 20140906165420) do
     t.boolean  "show",                   default: true
     t.json     "groups"
     t.boolean  "skip_consolation_round"
+    t.integer  "passing_score",          default: 0
   end
 
   create_table "users", force: true do |t|
