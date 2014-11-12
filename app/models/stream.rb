f59119c6c2185ac2a6293877a9028f6858fac8f9
must-not-code <test4food@me.com>
2014-11-12 16:38:35 +0300

    get twitch banner url before save

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,5 +1,5 @@
 class Stream < ActiveRecord::Base
-  def receive_banner_url
-    self.update(banner_url: Twitch.channels.get(self.channel).video_banner_url)
+  before_save do
+    self.banner_url = Twitch.channels.get(self.channel).video_banner_url
   end
 end
