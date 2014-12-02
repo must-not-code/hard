8c495a7b703bd4c783fc7a2d9639b30eb8c4ef66
must-not-code <test4food@me.com>
2014-12-02 10:29:59 +0300

    fix assets config

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Hard
   class Application < Rails::Application
     config.time_zone = 'Moscow'
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
