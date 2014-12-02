8689f3531a851ffd67c1f40e8a105514906d37d6
must-not-code <test4food@me.com>
2014-12-02 17:59:35 +0300

    change default avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -38,14 +38,14 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  #def extension_white_list
-  #  %w(jpg jpeg gif png)
-  #end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    "#{secure_token}.png" if original_filename
+    "#{secure_token}.#{file.extension}" if original_filename
   end
 
   protected
