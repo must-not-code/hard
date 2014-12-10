d599334cb8cc347e863ca3904a25c79def297962
must-not-code <test4food@me.com>
2014-12-10 14:01:21 +0300

    refuctoring nav bar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    if moderator?
+    if logged_in? && current_user.group[/admin|newsman/]
       @posts = Post.order('published_at desc').page(params[:page]).per(10)
     else
       @posts = Post.where.not(published_at: nil).order('published_at desc').page(params[:page]).per(10)
@@ -12,71 +12,5 @@ class PostsController < ApplicationController
     gon.push({post_id: @post.id,
               post_path: post_path(@post.id),
               comments_path: comments_path})
-  end
-
-  def new
-    if moderator?
-      gon.push({post: {title: nil, game: nil, id: nil},
-                post_path: root_path,
-                post_save_path: post_save_path})
-      render 'edit.html.haml'
-    else
-      head 403
-    end
-  end
-
-  def edit
-    if moderator?
-      @post = Post.find(params[:id])
-      gon.push({post: @post,
-                post_path: post_path(@post.id),
-                post_save_path: post_save_path})
-      render 'edit.html.slim'
-    else
-      head 403
-    end
-  end
-
-  def save
-    if moderator?
-      if params[:post].empty?
-        @post = Post.create(user_id: current_user.id,
-                            title:   params[:title],
-                            game:    params[:game],
-                            content: params[:text].gsub('alt="zoom"', 'rel="zoom"'))
-      else
-        @post = Post.find(params[:post])
-        @post.update(title:   params[:title],
-                     game:    params[:game],
-                     content: params[:text].gsub('alt="zoom"', 'rel="zoom"'))
-      end
-
-      if @post.errors.empty?
-        render json: {success: true}
-      else
-        render json: {success: false,
-                      errors: @post.errors.first[1]}
-      end
-    else
-      head 403
-    end
-  end
-
-  def upload
-    if moderator?
-      img = Digest::SHA1.hexdigest(current_user.username + Time.now.to_i.to_s)[8..16]
-      path = 'public/posts/' + img + params[:file].original_filename[/\.[^.]+\z/]
-      File.open(path, "wb") do |file|
-        file.write(params[:file].read)
-      end
-      render text: "Ссылка на этот файл <strong>#{path[/(?<=public).*/]}</strong>"
-    else
-      head 403
-    end
-  end
-
-  private
-  def moderator?
-    logged_in? && current_user.group[/admin|newsman/]
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -6,12 +6,12 @@ class Team < ActiveRecord::Base
   validates :title,
     uniqueness: {case_sensitive: false,
       message: 'Команда с таким названием уже существует.'},
-    length: {in: 2..30,
-      message: 'Название должно содержать от 2 до 30 символов.'}
+    length: {in: 3..24,
+      message: 'Название должно содержать от 3 до 24 символов.'}
 
   validates :tag,
     uniqueness: {case_sensitive: false,
       message: 'Команда с таким тэгом уже существует.'},
-    length: {in: 2..10,
-      message: 'Тэг должен содержать от 2 до 10 символов.'}
+    length: {in: 2..6,
+      message: 'Тэг должен содержать от 2 до 6 символов.'}
 end
