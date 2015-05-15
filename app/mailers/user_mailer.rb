14afe9499e791964eab6fe7357fccc3d3cb3856c
must-not-code <test4food@me.com>
2015-05-15 16:08:38 +0300

    fix locale in urls in emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ActionMailer::Base
 
   def activation_needed_email(user)
     @user = user
-    @url  = "http://hardrandom.com/users/#{user.activation_token}/activate"
+    @url  = "http://hardrandom.com/#{I18n.locale}/users/#{user.activation_token}/activate"
     mail(to: user.email,
          subject: 'Регистрация на HardRandom.com')
   end
 
   def reset_password_email(user)
     @user = user
-    @url  = edit_password_reset_url(user.reset_password_token)
+    @url  = edit_password_reset_url(I18n.locale, user.reset_password_token)
     mail(to: user.email,
          subject: 'Сброс пароля на HardRandom.com')
   end
