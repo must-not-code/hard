713b694b0f62e0e68ce44b0cebcc1fb51729f681
must-not-code <test4food@me.com>
2015-12-05 19:21:48 +0300

    teams logo upload

---
+++ app/admin/team.rb
@@ -0,0 +1,40 @@
+ActiveAdmin.register Team do
+  permit_params :game, :title, :logo
+
+  index do
+    column :id
+    column :game
+    column :title
+    column :created_at
+    column :updated_at
+
+    actions
+  end
+
+  filter :game
+  filter :title
+  filter :created_at
+  filter :updated_at
+
+  show do |f|
+    attributes_table do
+      row :id
+      row :game
+      row :title
+      row :logo do
+        image_tag(f.logo.url)
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  form html: { multipart: true } do |f|
+    f.inputs do
+      f.input :game
+      f.input :title
+      f.input :logo, as: :file, hint: image_tag(object.logo.url)
+    end
+    f.actions
+  end
+end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ActiveRecord::Base
+  mount_uploader :logo, TeamLogoUploader
   has_many :members
 end
